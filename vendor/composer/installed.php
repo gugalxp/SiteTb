c8d0ca3bc135497112d1140e1081e955c8bb101e
Gustavo <gustavoleone3456@hotmail.com>
2021-09-16 14:30:56 -0300

    gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7014d5bcc8fc46aa775fe6c5a4dd43f723435156',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7014d5bcc8fc46aa775fe6c5a4dd43f723435156',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v5.2.28',
+            'version' => '5.2.28.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'acba50393dd03da69a50226c139722af8b153b11',
+            'dev_requirement' => false,
+        ),
+    ),
+);
